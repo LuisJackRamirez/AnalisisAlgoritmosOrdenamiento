69b8561f6c3f1486568729858efa192ad49e0357
luisthread <luisthread@gmail.com>
2020-10-25 10:38:20 -0600

    preparing for result in mathlab

--- startabb.sh
+++ startabb.sh
@@ -1,64 +1,22 @@
 echo "testing abb sort..."
-
-echo "test 100..."
-./abb 100 < nums.txt > abb-testing/abb-test100.txt
-
-echo "test 1000..."
-./abb 1000 < nums.txt > abb-testing/abb-test1000.txt
-
-echo "test 5000..."
-./abb 5000 < nums.txt > abb-testing/abb-test5000.txt
-
-echo "test 10000..."
-./abb 10000 < nums.txt > abb-testing/abb-test10000.txt
-
-echo "test 50000..."
-./abb 50000 < nums.txt > abb-testing/abb-test50000.txt
-
-echo "test 100000..."
-./abb 100000 < nums.txt > abb-testing/abb-test100000.txt
-
-echo "test 200000..."
-./abb 200000 < nums.txt > abb-testing/abb-test200000.txt
-
-echo "test 400000..."
-./abb 400000 < nums.txt > abb-testing/abb-test400000.txt
-
-echo "test 500000..."
-./abb 500000 < nums.txt > abb-testing/abb-test500000.txt
-
-echo "test 600000..."
-./abb 600000 < nums.txt > abb-testing/abb-test600000.txt
-
-echo "test 800000..."
-./abb 800000 < nums.txt > abb-testing/abb-test800000.txt
-
-echo "test 1000000..."
-./abb 1000000 < nums.txt > abb-testing/abb-test1000000.txt
-
-echo "test 2000000..."
-./abb 2000000 < nums.txt > abb-testing/abb-test2000000.txt
-
-echo "test 3000000..."
-./abb 3000000 < nums.txt > abb-testing/abb-test3000000.txt
-
-echo "test 4000000..."
-./abb 4000000 < nums.txt > abb-testing/abb-test4000000.txt
-
-echo "test 5000000..."
-./abb 5000000 < nums.txt > abb-testing/abb-test5000000.txt
-
-echo "test 6000000..."
-./abb 6000000 < nums.txt > abb-testing/abb-test6000000.txt
-
-echo "test 7000000..."
-./abb 7000000 < nums.txt > abb-testing/abb-test7000000.txt
-
-echo "test 8000000..."
-./abb 8000000 < nums.txt > abb-testing/abb-test8000000.txt
-
-echo "test 9000000..."
-./abb 9000000 < nums.txt > abb-testing/abb-test9000000.txt
-
-echo "test 10000000..."
-./abb 10000000 < nums.txt > abb-testing/abb-test10000000.txt
+./abb 100 < nums.txt
+./abb 1000 < nums.txt
+./abb 5000 < nums.txt
+./abb 10000 < nums.txt
+./abb 50000 < nums.txt
+./abb 100000 < nums.txt
+./abb 200000 < nums.txt
+./abb 400000 < nums.txt
+./abb 500000 < nums.txt
+./abb 600000 < nums.txt
+./abb 800000 < nums.txt
+./abb 1000000 < nums.txt
+./abb 2000000 < nums.txt
+./abb 3000000 < nums.txt
+./abb 4000000 < nums.txt
+./abb 5000000 < nums.txt
+./abb 6000000 < nums.txt
+./abb 7000000 < nums.txt
+./abb 8000000 < nums.txt
+./abb 9000000 < nums.txt
+./abb 10000000 < nums.txt
--- startshell.sh
+++ startshell.sh
@@ -1,64 +1,22 @@
-echo "testing shell sort..."
-
-echo "test 100..."
-./shell 100 < nums.txt > shell-testing/shell-test100.txt
-
-echo "test 1000..."
-./shell 1000 < nums.txt > shell-testing/shell-test1000.txt
-
-echo "test 5000..."
-./shell 5000 < nums.txt > shell-testing/shell-test5000.txt
-
-echo "test 10000..."
-./shell 10000 < nums.txt > shell-testing/shell-test10000.txt
-
-echo "test 50000..."
-./shell 50000 < nums.txt > shell-testing/shell-test50000.txt
-
-echo "test 100000..."
-./shell 100000 < nums.txt > shell-testing/shell-test100000.txt
-
-echo "test 200000..."
-./shell 200000 < nums.txt > shell-testing/shell-test200000.txt
-
-echo "test 400000..."
-./shell 400000 < nums.txt > shell-testing/shell-test400000.txt
-
-echo "test 500000..."
-./shell 500000 < nums.txt > shell-testing/shell-test500000.txt
-
-echo "test 600000..."
-./shell 600000 < nums.txt > shell-testing/shell-test600000.txt
-
-echo "test 800000..."
-./shell 800000 < nums.txt > shell-testing/shell-test800000.txt
-
-echo "test 1000000..."
-./shell 1000000 < nums.txt > shell-testing/shell-test1000000.txt
-
-echo "test 2000000..."
-./shell 2000000 < nums.txt > shell-testing/shell-test2000000.txt
-
-echo "test 3000000..."
-./shell 3000000 < nums.txt > shell-testing/shell-test3000000.txt
-
-echo "test 4000000..."
-./shell 4000000 < nums.txt > shell-testing/shell-test4000000.txt
-
-echo "test 5000000..."
-./shell 5000000 < nums.txt > shell-testing/shell-test5000000.txt
-
-echo "test 6000000..."
-./shell 6000000 < nums.txt > shell-testing/shell-test6000000.txt
-
-echo "test 7000000..."
-./shell 7000000 < nums.txt > shell-testing/shell-test7000000.txt
-
-echo "test 8000000..."
-./shell 8000000 < nums.txt > shell-testing/shell-test8000000.txt
-
-echo "test 9000000..."
-./shell 9000000 < nums.txt > shell-testing/shell-test9000000.txt
-
-echo "test 10000000..."
-./shell 10000000 < nums.txt > shell-testing/shell-test10000000.txt
+echo "Testing Shell Sort..."
+./shell 100 < nums.txt
+./shell 1000 < nums.txt
+./shell 5000 < nums.txt
+./shell 10000 < nums.txt
+./shell 50000 < nums.txt
+./shell 100000 < nums.txt
+./shell 200000 < nums.txt
+./shell 400000 < nums.txt
+./shell 500000 < nums.txt
+./shell 600000 < nums.txt
+./shell 800000 < nums.txt
+./shell 1000000 < nums.txt
+./shell 2000000 < nums.txt
+./shell 3000000 < nums.txt
+./shell 4000000 < nums.txt
+./shell 5000000 < nums.txt
+./shell 6000000 < nums.txt
+./shell 7000000 < nums.txt
+./shell 8000000 < nums.txt
+./shell 9000000 < nums.txt
+./shell 10000000 < nums.txt
