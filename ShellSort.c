34553bb1ac3aa9ccaa971ee077f9fec696da2cce
luisthread <luisthread@gmail.com>
2020-10-26 18:37:56 -0600

    Finish all code descriptions

--- ShellSort.c
+++ ShellSort.c
@@ -1,9 +1,31 @@
+/*
+    Titulo: Ordenamiento Shell
+    Descripción: Implemntación del algoritmo de ordenamiento Shell optimizado
+    Fecha: 26/10/2020
+    Version: 1.3
+    Autor: Luis Fernando Reséndiz Chávez
+    Observaciones: La implementación la tomé de un libro, ya que la del profesor no
+    funcionaba.
+*/
+
+/*********************************************************/
+        BIBLIOTECAS UTILIZADAS
+/*********************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "tiempo.h"
 
+/*********************************************************/
+        PROTOTIPOS DE FUNCIONES
+/*********************************************************/
+
 void ShellSort(int* a, int n);
+
+/*********************************************************/
+        FUNCIÓN PRINCIPAL DEL PROGRAMA
+/*********************************************************/
 
 int main(int argc, char *argv[]) {
     if(argc != 2)
@@ -36,6 +58,13 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/*********************************************************/
+        IMPLEMENTACIÓN DE FUNCIONES
+/*********************************************************/
+
+// ShellSort(int*, int) -> void
+// Recibe un apuntador al arreglo original a ordenar, y un entero que representa su tamaño.
+// Ordena los numeros del arreglo mediante el algoritmo de ShellSort.
 void ShellSort(int* a, int n) {
     int i, j, k, tmp;
 
